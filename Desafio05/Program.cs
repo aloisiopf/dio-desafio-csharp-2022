b34836ab9ac84f7ddf6792afec189c116a3fce04
aloisiopf <aloisiopf@gmail.com>
2022-02-28 21:59:28 -0300

    Desenvolvimento Desafio 05 - Jóias

--- Desafio05/Program.cs
+++ Desafio05/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+  
+  static void Main(string[] args) {
+    int qtd = 0, tamanho = 0;
+    List<string> joias = new List<string>();
+    IEnumerable<string> joias_distintas; //https://docs.microsoft.com/pt-br/dotnet/api/system.linq.enumerable.distinct?view=net-6.0
+    string joia = "";
+
+    do {
+      joia = Console.ReadLine();
+      tamanho += joia.Length;
+      
+      joias.Add(joia);
+      
+    } while(tamanho <= 106);
+    joias_distintas = joias.Distinct();
+
+    foreach (string x in joias_distintas){
+      qtd++;
+    }
+
+    Console.WriteLine(qtd);
+  }
+}
